38cd73cf76696dff73100846661fd369c612fd49
Max Barrass <max.barrass@gmail.com>
2020-07-21 01:39:23 +1000

    cleanup electron build config

--- vue.config.js
+++ vue.config.js
@@ -4,21 +4,9 @@ module.exports = {
     electronBuilder: {
       builderOptions: {
         directories: {
-          buildResources: "build",
-          output: "dist"
+          buildResources: "build"
         },
         asar: true,
-        files: [
-          "dist_electron/**/*",
-          "src/background.js",
-          "background.js",
-          "!**/node_modules/**/{CHANGELOG.md,README.md,README,readme.md,readme}",
-          "!node_modules/**/*.js.map",
-          "!node_modules/**/*.ts.map",
-          "!node_modules/**/{*.vcxproj,*.vcxproj.filters}",
-          "!node_modules/**/build/Release/{*.lib,*.exp,*.ilk,*.pdb,*.iobj,*.ipdb}",
-          "!node_modules/**/build/Release/obj"
-        ],
         win: {
           target: "nsis"
         },
