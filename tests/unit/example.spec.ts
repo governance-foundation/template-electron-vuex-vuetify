cc588395813f0882bea094a42f0027526c68be89
Max Barrass <max.barrass@gmail.com>
2020-07-20 22:26:46 +1000

    load vuetify into e2e

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,14 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+//load Vuetify to use in tests
+import Vue from "vue";
+import Vuetify from "vuetify";
+Vue.use(Vuetify);
+
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "Welcome to Vuetify";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
