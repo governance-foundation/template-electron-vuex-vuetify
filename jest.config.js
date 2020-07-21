050536b6b63a8fad22172c05835017a8ff282f0f
Max Barrass <max.barrass@gmail.com>
2020-07-22 09:49:06 +1000

    update path remove timeout

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,10 @@ module.exports = {
   collectCoverageFrom: [
     "**/*.{vue,ts,js}",
     "!**/node_modules/**",
-    "!<rootDir>/dist/**",
+    "!<rootDir>/dist_electron/**",
     "!<rootDir>/src/plugins/**",
     "!<rootDir>/tests/unit/**"
   ],
-  testTimeout: 300000,
   coverageReporters: ["json", "lcov", "text-summary"],
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
 };
