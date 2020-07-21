12ab7700823b7da11a6b6c555c8bb3a230d6b301
Max Barrass <max.barrass@gmail.com>
2020-07-21 10:14:42 +1000

    move config from command line

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{vue,ts,js}",
+    "!**/node_modules/**",
+    "!<rootDir>/dist/**",
+    "!<rootDir>/src/plugins/**",
+    "!<rootDir>/tests/unit/**"
+  ],
+  testTimeout: 300000,
+  coverageReporters: ["json", "lcov", "text-summary"],
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel"
 };
