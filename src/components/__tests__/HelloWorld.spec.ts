38ed5c6ded9bce9e701fa0be15a93f3cc1afa5ec
wildone <max.barrass@gmail.com>
2022-02-22 00:28:02 +1100

    unit tests are working.

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,21 @@
 import { describe, it, expect } from "vitest"
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
 
-import { mount } from "@vue/test-utils"
+import { mount, shallowMount } from "@vue/test-utils"
 import HelloWorld from "@/components/HelloWorld.vue"
 
 describe("HelloWorld", () => {
+  const vuetify = createVuetify({ components, directives })
+
   it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { props: { title: "Hello Vitest" } })
+    const wrapper = mount(HelloWorld, {
+      global: {
+        plugins: [vuetify],
+      },
+      props: { title: "Hello Vitest" },
+    })
     expect(wrapper.text()).toContain("Hello Vitest")
   })
 })
--- vitest/vuetify.ts
+++ vitest/vuetify.ts
@@ -1,6 +1,6 @@
 import "vuetify/lib/styles/main.sass"
 import { createVuetify } from "vuetify"
-import * as components from "vuetify/lib/components/index.mjs"
-import * as directives from "vuetify/lib/directives/index.mjs"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
 
 export default createVuetify({ components, directives })
