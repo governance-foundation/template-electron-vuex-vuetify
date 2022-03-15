27bdf606e99b5a3c391fe3a6ee678fd6ade7ac1f
wildone <max.barrass@gmail.com>
2022-03-16 00:41:41 +1100

    add unit tests for language selection.

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -2,20 +2,40 @@ import { describe, it, expect } from "vitest"
 import { createVuetify } from "vuetify"
 import * as components from "vuetify/components"
 import * as directives from "vuetify/directives"
+import i18n from "@/i18n"
 
 import { mount, shallowMount } from "@vue/test-utils"
 import HelloWorld from "@/components/HelloWorld.vue"
 
+export const waitFor = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms || 0))
+}
+
 describe("HelloWorld", () => {
   const vuetify = createVuetify({ components, directives })
 
-  it("renders properly", () => {
+  it("renders properly in english", () => {
     const wrapper = mount(HelloWorld, {
       global: {
-        plugins: [vuetify],
+        plugins: [vuetify, i18n],
       },
-      props: { title: "Hello Vitest" },
     })
-    expect(wrapper.text()).toContain("Hello Vitest")
+    expect(wrapper.text()).toContain("Welcome to Vue3")
+  })
+
+  it("renders properly in hindi", async () => {
+    const wrapper = mount(HelloWorld, {
+      global: {
+        plugins: [vuetify, i18n],
+      },
+    })
+
+    const langsel = wrapper.find("select")
+    langsel.element.value = "hi"
+    await langsel.trigger("change")
+
+    expect(wrapper.text()).toContain(
+      "Vue3 + Vite + Vuetify इलेक्ट्रॉन ऐप में आपका स्वागत है।"
+    )
   })
 })
